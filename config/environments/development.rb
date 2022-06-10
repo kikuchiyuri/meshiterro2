28d9b5d5cc415b8b596a866992d3df5405eebfef
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-06-10 13:02:01 +0000

    ActiveStrage設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "d3f4005603d6411f8d1c0d5b50e8cbfe.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
 end
